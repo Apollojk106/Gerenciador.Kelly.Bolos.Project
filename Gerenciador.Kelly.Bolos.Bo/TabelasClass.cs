9134cfa3bc1f52333348e7ec89812113691e3f68
Apollo <149687810+Apollojk106@users.noreply.github.com>
2024-06-12 22:26:58 -0300

    Create TabelasClass.cs

---
+++ Gerenciador.Kelly.Bolos.Bo/TabelasClass.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gerenciador.Kelly.Bolos.Bo;
+
+namespace Gerenciador.Kelly.Bolos.Bo
+{
+    public class TabelasClass
+    {
+        private string filtro { get; set; }
+
+        private string resultados { get; set; }
+
+        public TabelasClass() 
+        {
+            
+        }
+
+        public List<string> BuscarResultado(string filtro)
+        {   
+            BancoDeDadosClass bancoDeDadosClass = new BancoDeDadosClass();
+
+            List<string> filtros = bancoDeDadosClass.ObterFiltros(filtro);
+      
+            return filtros;
+        }
+    }
+}
