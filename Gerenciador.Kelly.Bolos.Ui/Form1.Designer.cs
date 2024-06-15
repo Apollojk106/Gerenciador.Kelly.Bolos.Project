f857613312811e7bf4cf4fd5487f2600ee20dea2
Apollojk106 <jk586622@gmail.com>
2024-06-14 22:44:46 -0300

    Login Melhorado

--- Gerenciador.Kelly.Bolos.Ui/Form1.Designer.cs
+++ Gerenciador.Kelly.Bolos.Ui/Form1.Designer.cs
@@ -43,7 +43,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(359, 279);
+            this.label1.Location = new System.Drawing.Point(452, 334);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(85, 32);
             this.label1.TabIndex = 0;
@@ -59,23 +59,26 @@
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(252, 325);
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(346, 382);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(299, 22);
+            this.txtEmail.Size = new System.Drawing.Size(299, 27);
             this.txtEmail.TabIndex = 2;
             // 
             // txtSenha
             // 
-            this.txtSenha.Location = new System.Drawing.Point(252, 363);
+            this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(346, 425);
             this.txtSenha.Name = "txtSenha";
-            this.txtSenha.Size = new System.Drawing.Size(299, 22);
+            this.txtSenha.Size = new System.Drawing.Size(299, 27);
             this.txtSenha.TabIndex = 3;
             // 
             // btnEnviar
             // 
-            this.btnEnviar.Location = new System.Drawing.Point(252, 400);
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(346, 491);
             this.btnEnviar.Name = "btnEnviar";
-            this.btnEnviar.Size = new System.Drawing.Size(299, 23);
+            this.btnEnviar.Size = new System.Drawing.Size(299, 32);
             this.btnEnviar.TabIndex = 4;
             this.btnEnviar.Text = "Logar";
             this.btnEnviar.UseVisualStyleBackColor = true;
@@ -85,9 +88,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Location = new System.Drawing.Point(192, 328);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(286, 383);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.Size = new System.Drawing.Size(54, 22);
             this.label3.TabIndex = 5;
             this.label3.Text = "Email";
             // 
@@ -95,9 +99,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Location = new System.Drawing.Point(192, 366);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(278, 426);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 16);
+            this.label4.Size = new System.Drawing.Size(62, 22);
             this.label4.TabIndex = 6;
             this.label4.Text = "Senha";
             // 
@@ -105,7 +110,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(755, 0);
+            this.button1.Location = new System.Drawing.Point(936, 0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(46, 37);
             this.button1.TabIndex = 7;
@@ -119,7 +124,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Gerenciador.Kelly.Bolos.Ui.Properties.Resources.Group_78;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(981, 558);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
