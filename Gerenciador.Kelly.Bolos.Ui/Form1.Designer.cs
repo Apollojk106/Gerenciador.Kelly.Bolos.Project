7f0edfcc1ca87c51c7ceb70bef928a8a1dc940ff
Apollojk106 <jk586622@gmail.com>
2024-06-20 10:47:06 -0300

    char password

--- Gerenciador.Kelly.Bolos.Ui/Form1.Designer.cs
+++ Gerenciador.Kelly.Bolos.Ui/Form1.Designer.cs
@@ -70,6 +70,7 @@
             this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtSenha.Location = new System.Drawing.Point(346, 425);
             this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
             this.txtSenha.Size = new System.Drawing.Size(299, 27);
             this.txtSenha.TabIndex = 3;
             // 
